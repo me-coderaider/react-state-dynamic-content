cf9ea269dfaf4bbab8a75fadd306f743268c3381
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-12 23:46:10 +0530

    added state to App component to count animals

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-function App() {
-    // variation 1:
-    // const handleClick = () => {
-    //     console.log("button clicked");
-    // };
+import { useState } from "react";
 
-    // variation 2:
-    // const handleClick = function () {
-    //     console.log("button clicked");
-    // };
+function App() {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div>
-            {/* <button onClick={handleClick}>Add Animal</button> */}
-            {/** variation 3: */}
-            {/* <button
-                onClick={function () {
-                    console.log("button clicked");
-                }}
-            >
-                Add Animal
-            </button> */}
-
-            {/* variation 4: */}
-            {/* <button
-                onClick={() => {
-                    console.log("button clicked");
-                }}
-            >
-                Add Animal
-            </button> */}
-
-            {/* variation 5: */}
-            <button onClick={() => console.log("button clicked")}>
-                Add Animal
-            </button>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
         </div>
     );
 }
